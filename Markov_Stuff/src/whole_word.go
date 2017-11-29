ae4ebefdc69acda70483d7ca4b2249605d3f7938
Ben <i@auburn.edu>
2017-11-28 19:39:45 -0600

    can now produce letters using a kind of random method, next to read multiple letters

--- Markov_Stuff/src/whole_word.go
+++ Markov_Stuff/src/whole_word.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+  "math/rand"
   "unicode"
+  "errors"
   "bufio"
   "fmt"
   "log"
@@ -10,7 +12,13 @@ import (
 
 func main() {
   lines := read_file("./../storage/books/PandP.txt", 26)
-  read_by_letter(lines)
+  letter_map := read_by_letter(lines)
+
+  for _, v := range letter_map {
+    l := pick_next_letter_index(v)
+    fmt.Printf("%c\n", l)
+  }
+
 }
 
 func read_file(filename string, lines_to_skip int) []string {
@@ -31,33 +39,83 @@ func read_file(filename string, lines_to_skip int) []string {
   return lines
 }
 
-func read_by_letter(lines []string) {
+/*
+func random(min, max int) int {
+  rand.Seed(time.Now().Unix())
+  return rand.Intn(max - min) + min
+}
+
+*/
+
+// Used by pick_next_letter_index
+func get_letter_from_index(index int) rune {
+  switch index {
+    case 26:
+      return '.'
+    case 27:
+      return ' '
+    default:
+      return rune(index) + 97
+  }
+}
+
+func pick_next_letter_index(char_counts [28]int) rune {
+  count := 0
+
+  for _, val := range char_counts {
+    count += val
+  }
+
+  random_number := rand.Intn(count)
+
+  for i, val := range char_counts {
+    random_number -= val
+    if random_number <= 0 {
+      return get_letter_from_index(i)
+    }
+  }
+
+  if random_number > 0 {
+    check_err(errors.New("Somehow we did not reach 0 in picking the next letter"))
+  }
+  return 'a';
+}
+
+func read_by_letters(lines []string, letter_count int) map[string][]int {
+  return nil
+}
+
+func read_by_letter(lines []string) map[rune][28]int {
   var char_map = make(map[rune][28]int)
-  var current_letter rune
+
+  current_index := 'a' - 97
 
   for _, line := range lines {
     if len(line) == 0 { continue }
     for _, char := range line {
       if char == '.' {
-        letter_count := char_map['.']
+        letter_count := char_map[current_index]
         letter_count[26]++
-        char_map['.'] = letter_count
-        current_letter = '.'
+        char_map[current_index] = letter_count
+        current_index = 26
       } else if char == ' ' {
-        letter_count := char_map[' ']
+        letter_count := char_map[current_index]
         letter_count[27]++
-        char_map[' '] = letter_count
-        current_letter = ' '
+        char_map[current_index] = letter_count
+        current_index = 27
       } else if unicode.IsLetter(char) {
         char = unicode.ToLower(char)
-        letter_count := char_map[char - 97]
+        letter_count := char_map[current_index]
         letter_count[char - 97]++
-        char_map[char - 97] = letter_count
-        current_letter = char - 97
+        char_map[current_index] = letter_count
+        current_index = char - 97
       }
     }
   }
+  return char_map
+}
 
+func print_map(char_map map[rune][28]int) {
   for k, c := range char_map {
     fmt.Printf("%c", k + 97)
     fmt.Println(" ->", c)
