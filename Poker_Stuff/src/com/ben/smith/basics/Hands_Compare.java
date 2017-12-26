a7a398b74b7d3e6cc8c3e5c35173242658310d60
Ben <i@auburn.edu>
2017-12-26 17:01:27 -0600

    I just realized that I need to think about how I am going to compare hands some more.

---
+++ Poker_Stuff/src/com/ben/smith/basics/Hands_Compare.java
@@ -0,0 +1,60 @@
+package com.ben.smith.basics;
+
+/**
+ * Created by bensmith on 12/26/17.
+ */
+public class Hands_Compare {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+    public int compare(Card[] a, Card[] b) {
+        return 0;
+    }
+
+    public boolean royal_flush_check() {
+        return false;
+    }
+
+    public boolean straight_flush_check() {
+        return false;
+    }
+
+    public boolean four_of_a_kind_check() {
+        return false;
+    }
+
+    public boolean full_house_check() {
+        return false;
+    }
+
+    public boolean flush_check() {
+        return false;
+    }
+
+    public boolean straight_check() {
+        return false;
+    }
+
+    public boolean three_of_a_kind_check() {
+        return false;
+    }
+
+    public boolean two_pair_check() {
+        return false;
+    }
+
+    public boolean pair() {
+        return false;
+    }
+
+    public boolean high_card_check() {
+        return false;
+    }
+
+
+}
