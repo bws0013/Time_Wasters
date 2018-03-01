ad6abdc53298102b290a3efd482b46b26032a270
Ben <i@auburn.edu>
2018-03-01 06:48:06 -0600

    Songs generated

--- Song_Distance/src/calculate.go
+++ Song_Distance/src/calculate.go
@@ -4,6 +4,19 @@ import (
   "fmt"
 )
 
-func main() {
+/*
+The first part is going to involve a target that is standing still.
 
+We will be given a song list and a number of seconds that will never change.
+Any number of songs can be used.
+*/
+
+
+
+func main() {
+  sl := generate_songs(20)
+
+  for _, s := range sl {
+    fmt.Println(s.Length)
+  }
 }
--- Song_Distance/src/song.go
+++ Song_Distance/src/song.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+  "fmt"
   "log"
+  "math/rand"
+  "strconv"
 )
 
 // This file holds our struct and some boiler plate methods like error checking
@@ -16,6 +19,26 @@ type Song struct {
   Name    string  `json:"name"`
   Length  int  `json:"length"`
   Played  bool  `json:"played"`
+}
+
+var (
+  song_length_min = 150
+  song_length_max = 300
+)
+
+// Generates a list of N songs
+func generate_songs(song_count int) []Song {
+  song_list := make([]Song, 0)
+
+  rand.Seed(42) // Make this dynamic later // rand.Seed(time.Now().Unix())
+
+  for i := 0; i < song_count; i++ {
+    name := strconv.Itoa(i)
+    length := rand.Intn(song_length_max - song_length_min) + song_length_min
+    song_list = append(song_list, Song{name, length, false})
+  }
+
+  return song_list
 }
 
 // Simple error check
